3dd5b49eda34887ff88bc8668011388514e0168d
Abdul Akhundzada <abdul.akhundzadeh@gmail.com>
2025-07-01 11:09:59 +0300

    Replaced fseek with diskseek to wrap the system call in case disk is corrupted. Added additional log message for clarity.

--- src/iodisk.c
+++ src/iodisk.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <string.h>
 
+#include "err.h"
 #include "fs.h"
 #include "logging.h"
 
@@ -16,21 +17,21 @@ extern uint8_t bitmap[];
 void read_inode(int inode_no, Inode* inode) {
   assert_disk_was_mounted();
   logMsg(INFO_LOG, "Reading Inode # %d", inode_no);
-  fseek(disk, INODE_START * BLOCK_SIZE + sizeof(Inode) * inode_no, SEEK_SET);
+  diskseek(disk, INODE_START * BLOCK_SIZE + sizeof(Inode) * inode_no, SEEK_SET);
   fread(inode, sizeof(Inode), 1, disk);
 }
 
 void write_inode(int inode_no, Inode inode) {
   assert_disk_was_mounted();
   logMsg(INFO_LOG, "Writing to Inode # %d", inode_no);
-  fseek(disk, INODE_START * BLOCK_SIZE + sizeof(Inode) * inode_no, SEEK_SET);
+  diskseek(disk, INODE_START * BLOCK_SIZE + sizeof(Inode) * inode_no, SEEK_SET);
   fwrite(&inode, sizeof(Inode), 1, disk);
 }
 
 void init_inode_table() {
   assert_disk_was_mounted();
   Inode inode = {0};
-  fseek(disk, INODE_START * BLOCK_SIZE, SEEK_SET);
+  diskseek(disk, INODE_START * BLOCK_SIZE, SEEK_SET);
   for (int i = 0; i < MAX_INODES; ++i) {
     fwrite(&inode, sizeof(Inode), 1, disk);
   }
@@ -41,8 +42,8 @@ int alloc_inode() {
   logMsg(INFO_LOG, "Allocating an Inode");
   Inode inode;
   // In `alloc_inode()`, skip using `read_inode()`
-  // to prevent repeated `fseek` operations.
-  fseek(disk, INODE_START * BLOCK_SIZE, SEEK_SET);
+  // to prevent repeated `diskseek` operations.
+  diskseek(disk, INODE_START * BLOCK_SIZE, SEEK_SET);
   // Go over each pre-allocated Inode
   // and find one that's free to use.
   for (int i = 0; i < MAX_INODES; ++i) {
@@ -54,6 +55,7 @@ int alloc_inode() {
       return i;
     }
   }
+  logMsg(INFO_LOG, "Failed to allocate a free inode.");
   return -1;
 }
 
@@ -101,7 +103,6 @@ void add_dirent(int parent_inode_no, DirectoryEntry dirent) {
         logMsg(INFO_LOG, "Allocating data block.");
         int block_no = alloc_block();
         if (block_no < 0) {
-          logMsg(ERROR_LOG, "Failed to allocate new data block.");
           return;
         }
         inode.data_blocks[nblocks] = block_no;
@@ -123,33 +124,33 @@ void add_dirent(int parent_inode_no, DirectoryEntry dirent) {
 //* --SuperBlock
 void load_superblock() {
   assert_disk_was_mounted();
-  fseek(disk, 0, SEEK_SET);
+  diskseek(disk, 0, SEEK_SET);
   fread(&sb, sizeof(SuperBlock), 1, disk);
 }
 
 void write_superblock() {
   assert_disk_was_mounted();
-  fseek(disk, 0, SEEK_SET);
+  diskseek(disk, 0, SEEK_SET);
   fwrite(&sb, sizeof(SuperBlock), 1, disk);
 }
 
 //* --Bitmap & Data Blocks
 void load_bitmap() {
   assert_disk_was_mounted();
-  fseek(disk, BITMAP_START * BLOCK_SIZE, SEEK_SET);
+  diskseek(disk, BITMAP_START * BLOCK_SIZE, SEEK_SET);
   fread(bitmap, 1, BLOCK_SIZE, disk);
 }
 
 void clear_bitmap() {
   assert_disk_was_mounted();
   memset(bitmap, 0, BLOCK_SIZE);
-  fseek(disk, BITMAP_START * BLOCK_SIZE, SEEK_SET);
+  diskseek(disk, BITMAP_START * BLOCK_SIZE, SEEK_SET);
   fwrite(bitmap, BLOCK_SIZE, 1, disk);
 }
 
 void write_bitmap() {
   assert_disk_was_mounted();
-  fseek(disk, BITMAP_START * BLOCK_SIZE, SEEK_SET);
+  diskseek(disk, BITMAP_START * BLOCK_SIZE, SEEK_SET);
   fwrite(bitmap, 1, BLOCK_SIZE, disk);
 }
 
@@ -162,6 +163,7 @@ int alloc_block() {
       return i;
     }
   }
+  logMsg(ERROR_LOG, "Failed to allocate a free data block.");
   return -1;
 }
 
@@ -185,7 +187,7 @@ void set_bmp(int block_no, char flag) {
 
 void read_data_block(int block_no, void* buf, size_t size) {
   assert_disk_was_mounted();
-  fseek(disk, block_no * BLOCK_SIZE, SEEK_SET);
+  diskseek(disk, block_no * BLOCK_SIZE, SEEK_SET);
   fread(buf, 1, size, disk);
 }
 
@@ -195,11 +197,28 @@ void write_data_block(int block_no, void* data, size_t size) {
     logMsg(ERROR_LOG, "write_data_block: size exceeds BLOCK_SIZE");
     return;
   }
-  fseek(disk, block_no * BLOCK_SIZE, SEEK_SET);
+  diskseek(disk, block_no * BLOCK_SIZE, SEEK_SET);
   fwrite(data, 1, size, disk);
+}
+
+void write_blocks(Inode inode, void* data, size_t size) {
+  assert_disk_was_mounted();
+  // int starting_block = inode.size / BLOCK_SIZE;
+  // int nblocks = (size + BLOCK_SIZE - 1) / BLOCK_SIZE;
+  // int extra_bytes = inode.size % BLOCK_SIZE;
+  // for (int i = starting_block; i < nblocks; i++) {
+  //   write_data_block(inode.data_blocks[i], (void*)data, BLOCK_SIZE);
+  //   data += BLOCK_SIZE;
+  // }
 }
 
 bool block_is_free(int block_no) {
   assert_disk_was_mounted();
   return !(bitmap[block_no / 8] & (1 << (block_no % 8)));
+}
+
+void diskseek(FILE* f, long pos, int start) {
+  if (fseek(f, pos, start) != 0) {
+    err_exit("Disk is corrupted.");
+  }
 }
